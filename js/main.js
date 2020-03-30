8c42233d3bb5d923b8e65e144d22a491a49a6add
Carmine Lentisco <carminelentisco@gmail.com>
2020-03-30 15:34:43 +0200

    Esercizio 1 Logica completata

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Cotrollo inserimento JS
+
+console.log('JS è stato inserito correttamente');
+
+/********** Esercizio 1 **********/
+
+// Dati iniziali
+
+var listEmail = ["michele@boolean.careers", "fabio@boolean.careers", "roberto@boolean.careers"]
+var userEmail = prompt('Inserisci qui la tua E-mail : ');
+var verifyEmail = false;
+// Controllo E-mail
+
+for (i = 0; i < listEmail.length; i++) {
+
+    // Dichiarazione variabile di controllo
+    var checkEmail =  listEmail[i]; 
+    
+    // Verifica uguaglianza E-mail 
+    if (checkEmail == userEmail) {
+       verifyEmail = true;
+    } 
+    
+}
+
+// Avviso 
+
+if (verifyEmail == true) {
+    console.log('Verificata');
+} else {
+    console.log('E-mail Rifiutata');
+}
